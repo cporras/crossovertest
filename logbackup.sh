eaa527592f47f66e14d1146c530444f539946528
Cristian Porras <porrascristian@gmail.com>
2016-09-11 17:25:33 +0000

    Add sudo

--- logbackup.sh
+++ logbackup.sh
@@ -1,5 +1,5 @@
-docker run --rm --volumes-from apache -v ~/backups:/backup debian:8.5 tar -czvf /backup/apachelogs.tar.gz -C /usr/local/apache2/logs/ .
-docker run --rm --volumes-from mysql -v ~/backups:/backup debian:8.5  tar -czvf /backup/mysqllogs.tar.gz -C /var/log/mysql/ .
+sudo docker run --rm --volumes-from apache -v ~/backups:/backup debian:8.5 tar -czvf /backup/apachelogs.tar.gz -C /usr/local/apache2/logs/ .
+sudo docker run --rm --volumes-from mysql -v ~/backups:/backup debian:8.5  tar -czvf /backup/mysqllogs.tar.gz -C /var/log/mysql/ .
 current_date=$(date +"%Y%m%d_%H%M%S")
 mkdir ~/backups/$current_date/
 mv  ~/backups/apachelogs.tar.gz ~/backups/$current_date/
